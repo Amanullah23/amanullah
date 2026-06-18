7e7a8d16c108748674ce6b9c2c8accac1a318665
Amanullah23 <amanyawari220@gmail.com>
2026-06-18 08:27:57 +0430

    some changes applied

--- assets/assets.js
+++ assets/assets.js
@@ -131,7 +131,7 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'PHP, Python, JavaScript, React Js, Dart, SQL' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'JavaScript, TypeScript, Python, Dart, SQL, PHP' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Bachelor of Computer Science, Kabul University' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 20 projects' }
 ];
